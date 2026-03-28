083daa3c1d0fa4a03b9b5cf475a63e6f45d4deff
vincentfranky50-ui <vincentfranky50@gmail.com>
2026-03-28 06:50:19 +0100

    Update setting.js

--- setting.js
+++ setting.js
@@ -4,9 +4,9 @@
 
 module.exports = {
   BOT_NAME: process.env.BOT_NAME || 'ᴘᴇᴀᴋʏ-ʙʟɪɴᴅᴇʀ-ᴍᴅ', 
-  OWNER_NAME: process.env.OWNER_NAME || 'Mr Lee', 
-  OWNER_NUMBER: process.env.OWNER_NUMBER || '254116266407', 
-  SESSION_ID: process.env.SESSION_ID || '', // paste here your blinders session ID
+  OWNER_NAME: process.env.OWNER_NAME || 'Mr blink', 
+  OWNER_NUMBER: process.env.OWNER_NUMBER || '237658687623', 
+  SESSION_ID: process.env.SESSION_ID || 'blinder~67BkACyJ#Brhm96tvtHggIQrQkeNXB8bDYOb_Zlu7ffIWewy6Jys', // paste here your blinders session ID
   MODE: process.env.MODE || "private", // public or private
   TIMEZONE: process.env.TIMEZONE || 'Africa/Nairobi' 
 };
